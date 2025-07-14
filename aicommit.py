086da244b5b68ae130578a19fee554df122350f6
Darío Clavijo <clavijodario@gmail.com>
2025-07-14 15:47:47 -0300

    Update AI model configuration and commit message generation logic

--- aicommit.py
+++ aicommit.py
@@ -1,9 +1,26 @@
 #!/usr/bin/python3
 
-import subprocess
-import openai
+
+
 import os
+import re
+import openai
 import argparse
+import subprocess
+
+
+BASE_URL = "https://api.groq.com/openai/v1"
+#MODEL="meta-llama/llama-4-maverick-17b-128e-instruct"
+#MODEL="meta-llama/llama-4-scout-17b-16e-instruct"
+#MODEL="meta-llama/llama-guard-4-12b"
+#MODEL="llama3-70b-8192"
+MODEL="llama3-8b-8192"
+#MODEL="llama-3.3-70b-versatile"
+#MODEL="gemma2-9b-it"
+#MODEL="qwen/qwen3-32b"
+#MODEL="qwen-qwq-32b"
+#MODEL="llama-3.1-8b-instant"
+MODEL="deepseek-r1-distill-llama-70b"
 
 def get_git_diff(filename=None):
     """Gets the Git diff of unstaged and staged changes."""
@@ -21,17 +38,18 @@ def get_git_diff(filename=None):
 
 def generate_commit_message(diff_text):
     """Generates a commit message using OpenAI's GPT."""
-    prompt = f"Summarize the following Git diff and generate a concise and meaningful commit message in less than fifty words:\n\n{diff_text}"
-    
+    prompt = f"Summarize the following Git diff as a commit message. Output only the commit message, nothing else.\n\n{diff_text}"    
     try:
-        client = openai.OpenAI(base_url="http://localhost:1234/v1", api_key="not-needed")  # Local LLM
+        client = openai.OpenAI(base_url=BASE_URL, api_key=os.environ['API_KEY'])  # Local LLM
         response = client.chat.completions.create(
-            model="local-model",  # Not using gpt-4o for local
+            model=MODEL,  # Not using gpt-4o for local
             messages=[{"role": "system", "content": "You are a helpful assistant skilled in writing Git commit messages."},
                       {"role": "user", "content": prompt}],
-            max_tokens=100,
+            max_tokens=2000
         )
-        return response.choices[0].message.content.strip().replace('"','').replace("Commit Message:","")
+        msg = response.choices[0].message.content.strip()
+        msg = cleaned = re.sub(r'<think>.*?</think>', '', msg, flags=re.DOTALL)
+        return msg
     except Exception as e:
         print(f"Error generating commit message: {e}")
         return None
